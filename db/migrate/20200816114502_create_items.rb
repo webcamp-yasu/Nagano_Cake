9da4e9b144245aaded8997ddfadb46046e8d9b1e
yuutarou <yuutarouisyokaniseboy1@gmail.com>
2020-08-16 22:25:17 +0900

    Itemsモデル作成

---
+++ db/migrate/20200816114502_create_items.rb
@@ -0,0 +1,15 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.timestamps
+      t.string :name, null: false
+      t.text :detail, null: false
+      t.integer :genre_id, null: false
+      t.integer :price, null: false
+      t.string :image_id, null: false
+      t.boolean :is_active, null: false, default: true
+      t.datetime :created_at, null: false, default: "row"
+      t.datetime :update_at, null: false, default: "row"
+    end
+  end
+end
