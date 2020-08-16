36fa25fcc65e3ea68323a25ca9be82fa26be3c13
vagrant <vagrant@localhost.localdomain>
2020-08-16 12:38:58 +0000

    [admin-orders]Add model, controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_16_112751) do
+ActiveRecord::Schema.define(version: 2020_08_16_123417) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "customer_id"
@@ -38,6 +38,19 @@ ActiveRecord::Schema.define(version: 2020_08_16_112751) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id"
+    t.integer "postage"
+    t.integer "billing_amount"
+    t.integer "payment_method"
+    t.string "postal_code"
+    t.string "address"
+    t.string "address_name"
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
