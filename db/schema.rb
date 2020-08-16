1d526c88d37c6532d243242ab93f5316c41af81b
vagrant <vagrant@localhost.localdomain>
2020-08-16 13:28:33 +0000

    Itemsモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_16_080959) do
+ActiveRecord::Schema.define(version: 2020_08_16_114502) do
 
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,6 +30,18 @@ ActiveRecord::Schema.define(version: 2020_08_16_080959) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name", null: false
+    t.text "detail", null: false
+    t.integer "genre_id", null: false
+    t.integer "price", null: false
+    t.string "image_id", null: false
+    t.boolean "is_active", default: true, null: false
+    t.datetime "update_at", null: false
   end
 
 end
