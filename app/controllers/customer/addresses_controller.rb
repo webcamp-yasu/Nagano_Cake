0cb519725714fcc01eb15a52ab84dfca07e22617
shiseki toshikazu <uw-t.a.k.n.s-10ve@ezweb.ne.jp>
2020-08-17 07:22:06 +0000

    controller action teigi

--- app/controllers/customer/addresses_controller.rb
+++ app/controllers/customer/addresses_controller.rb
@@ -1,17 +1,54 @@
 class Customer::AddressesController < ApplicationController
 
+	#配送先一覧ページ・登録ページ
 	def index
+		@address = Address.new
+		@addresses = Address.all
 	end
 
+	#配送先新規登録アクション
 	def create
+		@address = Address.new
+		@address.customer_id = current_customer.id
+		if @address.save
+			redirect_to address_path(@address)
+		else
+			@address.customer_id = current_customer.id
+			@addresses = Address.all
+			render 'index'
+		end
 	end
 
-	def destroy
-	end
-
+	#配送先編集ページ
 	def edit
+		@address = Address.find(params[:id])
+		@customer = @address.customer
+		if @customer != current_customer
+			redirect_to addresses_path
+		end
 	end
 
+	#配送先編集アクション
 	def update
+		@address = Address.find(params[:id])
+		if @address.update(address_params)
+			redirect_to address_path(@address)
+		else
+			render "edit"
+		end
 	end
+
+	#配送先削除アクション
+	def destroy
+		@address = Address.find(params[:id])
+		@address.destroy
+		redirect_to addresses_path
+	end
+
+	private
+
+	def address_params
+		params.require(:address).permit(:name, :postal_code, :address)
+	end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2020_08_17_030503) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
+
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
