a19a483690629ee5756a2f5f8d8d0d5efe1d5248
vagrant <vagrant@localhost.localdomain>
2020-08-17 14:04:27 +0000

    [admin-homes]updated view and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
+
+  namespace :admin do
+    root 'homes#top'
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
