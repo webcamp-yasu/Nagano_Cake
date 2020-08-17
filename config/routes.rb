df4a9960e835d8334760cbaff342c226504fa4ba
vagrant <vagrant@localhost.localdomain>
2020-08-17 14:44:01 +0000

    [admin-homes]view, controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
-  
+
   root "customer/homes#top"
 
   get "customers/mypage" => "customer/customers#show"
   get "/about" => "customer/homes#about"
+
+  namespace :admin do
+    root 'homes#top'
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
