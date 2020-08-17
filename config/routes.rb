6da65b0908ce013faa8c55d9703c36b93e1a4cfd
Yuzuru <yuzuru@YuzurunoAir.AirPort>
2020-08-17 17:32:34 +0900

    Customer Model修正・Customer側のviewsドラフト・mypageの部分だけルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
+
+  get 'customers/mypage' => 'customer/customers#show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
