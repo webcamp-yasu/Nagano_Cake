83dd4a6ef0c3439394effdf1968e1f58d2a041d7
vagrant <vagrant@localhost.localdomain>
2020-08-17 14:23:24 +0000

    rever back due to mistake commit Revert "[admin-homes]updated view and controller"
    This reverts commit a19a483690629ee5756a2f5f8d8d0d5efe1d5248.

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,5 +1,2 @@
 class Admin::HomesController < ApplicationController
-	def top
-		@order = Order.where(created_at: Time.zone.now.all_day)
-	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
-
-  namespace :admin do
-    root 'homes#top'
-end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
