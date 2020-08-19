1c60e4ac0160881f6f1291820625ef5a80cdc332
shiseki toshikazu <uw-t.a.k.n.s-10ve@ezweb.ne.jp>
2020-08-19 07:41:56 +0000

    reiauto tuika

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,6 +2,6 @@ class Address < ApplicationRecord
 	#belongs_to :customer
 
 	validates :name, presence: true
-	validates :Address, presence: true
+	validates :address, presence: true
 	validates :postal_code, length: {is: 7}, numericality: {only_integer: true}
 end
