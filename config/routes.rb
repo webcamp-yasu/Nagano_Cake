19db6711d00fd69f56cce6865560455afe58e781
yuutarou <yuutarouisyokaniseboy1@gmail.com>
2020-08-19 14:54:56 +0900

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,48 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
-  
+
+  devise_for :admins, controllers: {
+    sessions: "admins/sessions"
+  }
+
+  devise_for :customers, controllers: {
+    registrations: "customers/registrations",
+    sessions: "customers/sessions"
+  }
+
+  #Customer
+
   root "customer/homes#top"
+  get "about" => "customer/homes#about"
 
-  get "customers/mypage" => "customer/customers#show"
-  get "/about" => "customer/homes#about"
+  scope module: :customer do
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    #デフォルトだとdeviseの編集ページに跳んでしまうので指定
+    get "customer/edit" => "customers#edit"
+    put "customer" => "customers#update"
+
+    resources :customers, only: [:show] do
+      collection do
+        get "mypage" => "customers#show"
+        get "quit"
+        patch "withdraw"
+      end
+    end
+    resources :items, omly: [:index,:show]
+    resources :cart_items
+    resources :orders
+    resources :addresses
+  end
+
+  #Admin
+
+  namespace :admin do
+    root 'homes#top'
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :items
+    resources :genres
+    resources :orders do
+      resource :order_details
+    end
+  end
+
 end
