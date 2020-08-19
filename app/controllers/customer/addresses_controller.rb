47220d0a3933dc53c338c4e18e4675a6f872f705
shiseki toshikazu <uw-t.a.k.n.s-10ve@ezweb.ne.jp>
2020-08-19 04:22:12 +0000

    view kannsei

--- app/controllers/customer/addresses_controller.rb
+++ app/controllers/customer/addresses_controller.rb
@@ -8,34 +8,39 @@ class Customer::AddressesController < ApplicationController
 
 	#配送先新規登録アクション
 	def create
-		@address = Address.new
-		@address.customer_id = current_customer.id
-		if @address.save
-			redirect_to address_path(@address)
-		else
-			@address.customer_id = current_customer.id
-			@addresses = Address.all
-			render 'index'
-		end
+		@address = Address.new(address_params)
+		#@address.customer_id = current_customer.id
+		#if
+		@address.save
+			redirect_to addresses_path
+		#else
+			#@address.customer_id = current_customer.id
+		#	@addresses = Address.all
+		#	render 'index'
+		#end
+	end
+
+	def show
 	end
 
 	#配送先編集ページ
 	def edit
 		@address = Address.find(params[:id])
-		@customer = @address.customer
-		if @customer != current_customer
-			redirect_to addresses_path
-		end
+		#@customer = @address.customer
+		#if @customer != current_customer
+			#redirect_to edit_address_path
+		#end
 	end
 
 	#配送先編集アクション
 	def update
 		@address = Address.find(params[:id])
-		if @address.update(address_params)
-			redirect_to address_path(@address)
-		else
-			render "edit"
-		end
+		#if
+		@address.update(address_params)
+			redirect_to addresses_path
+		#else
+			#render "edit"
+		#end
 	end
 
 	#配送先削除アクション
