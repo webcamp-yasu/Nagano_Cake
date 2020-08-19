e29479aaab8be39fe9e167b7f869767b5a3f1a9c
shiseki toshikazu <uw-t.a.k.n.s-10ve@ezweb.ne.jp>
2020-08-19 06:31:31 +0000

    validates tuika

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,7 @@
 class Address < ApplicationRecord
 	#belongs_to :customer
+
+	validates :name, presence: true
+	validates :Address, presence: true
+	validates :postal_code, length: {is: 7}, numericality: {only_integer: true}
 end
