f46c4a68ef62ca9367a088800de3636571e66b11
Yuzuru <yuzuru@YuzurunoAir.AirPort>
2020-08-20 01:54:19 +0900

    住所検索機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,25 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  #会員新規登録時のパス
+  def after_sign_up_path_for(resource)
+    mypage_customers_path
+  end
+
+  #ログイン時のパス
+  def after_sign_in_path_for(resource)
+    if customer_signed_in?
+      mypage_customers_path
+    else
+      admin_root_path
+    end
+  end
+
+  #ログアウト時のパス
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :phone_number])
   end
