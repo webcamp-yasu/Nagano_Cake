183744d418f1d96a119b6bcb92238461ec300981
vagrant <vagrant@localhost.localdomain>
2020-08-23 07:10:27 +0000

    order

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,9 +4,4 @@ class Order < ApplicationRecord
 
 	enum status:{ waitingforpayment: 0, confirmpayment: 1, makingitem: 2, prepareshipment: 3, confitmedshipment: 4 }, _prefix: true
 
-	def total_price
-		order_details.to_a.sum { |item| item.total_price }
-	end
-
-
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -4,9 +4,5 @@ class OrderDetail < ApplicationRecord
 
 	enum production_status:{ new: 0, waiting: 1, working: 2, done: 3 }, _prefix: true
 
-	def total_item_price
-		item.price * amount
-	end
-
 
 end
