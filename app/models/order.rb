c5b3ec144fb68797c2f47846ecae6ff23ef5c173
vagrant <vagrant@localhost.localdomain>
2020-08-25 02:45:30 +0000

    8/25

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,6 @@ class Order < ApplicationRecord
 	belongs_to :customer
 	has_many :order_details
 
-    enum payment_method:{"クレジットカード": 0, "銀行振込": 1}
+    enum payment_method:{ creditcard: 0,banktransfer: 1}
     enum status:{ waitingforpayment: 0, confirmpayment: 1, makingitem: 2, prepareshipment: 3, confitmedshipment: 4 }
 end
