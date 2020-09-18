139c626677d1ff7aa9c8b52819ccf56b494cd1f0
vagrant <vagrant@localhost.localdomain>
2020-09-18 16:15:41 +0000

    test

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -5,13 +5,13 @@ class Admin::OrderDetailsController < ApplicationController
 	def update
 		@order_detail = OrderDetail.find(params[:id])
 		@order_detail.update(order_detail_params)
-    
+
     #製作ステータスを1つ「製作中」にすると、注文ステータスが「製作中」に更新
     if params[:order_detail][:production_status] == "working"
       @order_detail.order.update(status: "makingitem")
       flash[:notice] = "製作ステータスと注文ステータスを更新しました"
     end
-    
+
 		#製作ステータスが全て「製作完了」→注文ステータス発送待ち
 		@all_order_details = @order_detail.order.order_details
 		@not_done_order_details = @all_order_details.where.not(production_status: "done")
